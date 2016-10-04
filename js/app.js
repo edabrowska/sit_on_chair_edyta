1a57ac8dd279feb6ca10d0b498aba0b364804739
editt-editt <editt.dabrowska@gmail.com>
2016-10-04 22:16:25 +0200

    slider not ready

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //slider photos;
   var sliderParent = document.querySelector('.slider_parent');
-  var sliderImg = sliderParent.querySelectorAll('.slider_container > .slider_chair > li > img');
+  var sliderImg = sliderParent.querySelectorAll('.slider_container > .slider_chair > li');
   var prevPhoto = sliderParent.querySelector('.slider_left');
   var nextPhoto = sliderParent.querySelector('.slider_right');
   var visiblePictureIdx = 0;
@@ -42,10 +42,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
     nextPhoto.addEventListener('click', function(){
       console.log('next clicked');
+
       sliderImg[visiblePictureIdx].classList.remove('visible');
       visiblePictureIdx += 1;
 
-      if(visiblePictureIdx >= 4){
+      if(visiblePictureIdx >= 3){
         visiblePictureIdx = visiblePictureIdx -1;
       }
 
@@ -92,16 +93,6 @@ document.addEventListener("DOMContentLoaded", function(){
       });
 
     }
-
-//transport checkbox - czy to o to chodzilo
-  var transport = document.querySelector('#transport');
-    transport.addEventListener('change', function(){
-        if(transport.checked){
-            var checkTrans = transport.getAttribute('data-transport-price');
-
-            transport.classList.add('active');
-        }
-    });
 
 //drop down lists
   var prodForm = document.querySelector('.form');
@@ -170,36 +161,85 @@ document.addEventListener("DOMContentLoaded", function(){
 
 //summary
   var panelLeft = document.querySelector('.panel_left');
+  var sumKind = panelLeft.querySelector('.title');
   var sumColor = panelLeft.querySelector('.color');
   var sumMat = panelLeft.querySelector('.pattern');
   var sumTrans = panelLeft.querySelector('.transport');
   var panelRight = document.querySelector('.panel_right');
+  var sumKindVal = panelRight.querySelector('.title');
   var sumColorVal = panelRight.querySelector('.color');
   var sumMatVal = panelRight.querySelector('.pattern');
   var sumTransVal = panelRight.querySelector('.transport');
-  var summaryVal = document.querySelector('.sum');
+  var summaryVal = document.querySelector('.sum > strong');
 
   var subListKind = kindList.querySelectorAll('.list_panel > li');
   var subListCol = colorList.querySelectorAll('.list_panel > li');
   var subListMat = materialList.querySelectorAll('.list_panel > li');
 
-console.log(sumColorVal);
-console.log(sumTransVal);
+  var kindPriceList = 0;
+  var kindNameList = '';
+  var colorPriceList = 0;
+  var colorNameList = '';
+  var materialPriceList = 0;
+  var materialNameList = '';
 
-  /*  for(var i = 0, len = subListKind.length; i < len; i++){
-      var kindPrice = subListKind[i].getAttribute('data-kind-price');
-      sumKindVal.innerText = kindPrice;
+    for(var i = 0, len = subListKind.length; i < len; i++){
+      subListKind[i].addEventListener('click', function(){
+        kindPriceList = this.dataset.kindPrice;
+        kindNameList = this.innerText;
+        sumKindVal.innerText = kindPriceList + ' zl';
+        sumKind.innerText = kindNameList;
+
+        showSum();
+      });
     }
-  */
-
     for(var i = 0, len = subListCol.length; i < len; i++){
       subListCol[i].addEventListener('click', function(){
-        var colorPrice = subListCol[i].getAttribute('data-kind-price');
-        sumColorVal.innerText = colorPrice;
-      });  
+        colorPriceList = this.dataset.colorPrice;
+        colorNameList = this.innerText;
+        sumColorVal.innerText = colorPriceList + ' zl';
+        sumColor.innerText = colorNameList;
+
+        showSum();
+      });
     }
     for(var i = 0, len = subListMat.length; i < len; i++){
-      var materialPrice = subListMat[i].getAttribute('data-kind-price');
-      sumMatVal.innerText = materialPrice;
+      subListMat[i].addEventListener('click', function(){
+        materialPriceList = this.dataset.materialPrice;
+        materialNameList = this.innerText;
+        sumMatVal.innerText = materialPriceList + ' zl';
+        sumMat.innerText = materialNameList;
+
+        showSum();
+      });
     }
+//transport checkbox
+  var transport = document.querySelector('#transport');
+  var checkTrans = transport.getAttribute('data-transport-price');
+
+    transport.addEventListener('change', function(){
+        if(transport.checked){
+          var checkTrans = transport.getAttribute('data-transport-price');
+          sumTransVal.innerText = checkTrans + ' zl';
+          sumTrans.innerText = 'Transport';
+          transport.classList.add('active');
+        } else {
+          sumTransVal.innerText = '';
+          sumTrans.innerText = '';
+          transport.classList.remove('active');
+        }
+
+        showSum();
+    });
+
+    var showSum = function(){
+      var generalPrice = parseInt(kindPriceList) + parseInt(colorPriceList) + parseInt(materialPriceList);
+  console.log(generalPrice);
+        if((transport.checked == true) && (generalPrice > 0)){
+          summaryVal.innerText = generalPrice + parseInt(checkTrans) + ' zl';
+        } else if((transport.checked != true) && (generalPrice > 0)){
+          summaryVal.innerText = generalPrice + parseInt(0) + ' zl';
+        }
+    }
+
 });
